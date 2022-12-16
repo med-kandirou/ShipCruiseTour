2245519fb2e676788aa5dde3a990254b1238da99
kandirou0 <simo.kandirou15@gmail.com>
2022-12-16 22:58:24 +0100

    update

--- ShipCruiseTour/app/views/reservation.php
+++ ShipCruiseTour/app/views/reservation.php
@@ -4,11 +4,11 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12">
-            <center><h1>Tickets</h1></center>
+            <h1 class="title_ticket">Tickets</h1>
+            <h6  class="title_ticket2">Reserve your tickets here</h6>
         </div>
     </div>
 
 </div>
-<h1 class="center">Tickets</h1>
 
 <?php require_once APPROOT.'/views/inc/footer.php'; ?>
