e203da3d925186e022ac7c71498c5a7e9184447f
kandirou0 <simo.kandirou15@gmail.com>
2022-12-19 09:58:04 +0100

    update

--- ShipCruiseTour/app/views/admin/navire.php
+++ ShipCruiseTour/app/views/admin/navire.php
@@ -1,10 +1,17 @@
 <?php require_once APPROOT.'/views/admin/header_admin.php'; ?>
 
 <div class="container-fluid">
-    <h1 class="text-center mt-5">navire</h1>
+    <h1 class="text-center mt-5">Navires</h1>
 </div>
 
-<div class="container text-center">           
+<div class="container mb-4">
+  <div class="row d-flex justify-content-between">
+    <h3 class="ml-3">Les informations des Navires</h3>
+    <button type="submit" class="btn btn-primary mr-3">Ajouter une navires</button>
+  </div>
+</div>
+
+<div class="container text-center mt-5">           
   <table class="table table-hover">
     <thead>
       <tr>
@@ -12,7 +19,6 @@
         <th>Nom</th>
         <th>Nombre de chambre</th>
         <th>Nombre de places</th>
-
       </tr>
     </thead>
     <tbody>
@@ -26,6 +32,26 @@
         <?php endforeach;?>
     </tbody>
   </table>
+</div>
+
+<div class="container d-none" id="addnavire">
+  <form action="">
+    <div class="mb-3 mt-3">
+      <label for="email" class="form-label">Nom de navire :</label>
+      <input type="email" class="form-control" id="email" placeholder="Nom de navire" name="nom">
+    </div>
+    <div class="mb-3">
+      <label for="pwd" class="form-label">Nombre de chambre :</label>
+      <input type="number" class="form-control" placeholder="Nombre de chambre" name="nbrchambre">
+    </div>
+    <div class="mb-3">
+      <label for="pwd" class="form-label">Nombre de places :</label>
+      <input type="number" class="form-control" id="pwd"placeholder="Nombre de places" name="nbrplace">
+    </div>
+    <div class="mb-3">
+      <button type="submit" class="btn btn-warning">Ajouter</button>
+    </div>
+  </form>
 </div>
 
 
--- ShipCruiseTour/app/views/admin/port.php
+++ ShipCruiseTour/app/views/admin/port.php
@@ -1,16 +1,23 @@
 <?php require_once APPROOT.'/views/admin/header_admin.php'; ?>
 
 <div class="container-fluid">
-    <h1 class="text-center mb-5">Port</h1>
+    <h1 class="text-center mb-5">Ports</h1>
 </div>
-<div class="container text-center">           
+
+<div class="container mb-4">
+  <div class="row d-flex justify-content-between">
+    <h3 class="ml-3">Les informations des Ports</h3>
+    <button type="submit" class="btn btn-primary mr-3">Ajouter un port</button>
+  </div>
+</div>
+
+<div class="container text-center">   
   <table class="table table-hover">
     <thead>
       <tr>
         <th>Numreo</th>
         <th>Nom</th>
         <th>Pays</th>
-
       </tr>
     </thead>
     <tbody>
@@ -25,6 +32,22 @@
   </table>
 </div>
 
+
+<div class="container d-none" id="addport">
+  <form action="">
+    <div class="mb-3 mt-3">
+      <label for="email" class="form-label">Nom de port :</label>
+      <input type="email" class="form-control" id="email" placeholder="Nom de navire" name="nom">
+    </div>
+    <div class="mb-3">
+      <label for="pwd" class="form-label">Pays :</label>
+      <input type="text" class="form-control" placeholder="Pays" name="pays">
+    </div>
+    <div class="mb-3">
+      <button type="submit" class="btn btn-warning">Ajouter</button>
+    </div>
+  </form>
+</div>
 
 
 <?php require_once APPROOT.'/views/inc/footer.php'; ?>
