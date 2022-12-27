697cdadac45f07b6fcb815c48ebe000c80f17a0b
kandirou0 <simo.kandirou15@gmail.com>
2022-12-27 16:50:13 +0100

    update

--- ShipCruiseTour/app/controllers/Reservations.php
+++ ShipCruiseTour/app/controllers/Reservations.php
@@ -4,9 +4,13 @@ class Reservations extends Controller{
     function __construct() {
     }
     
-    function reserver(){
+    function reserver($id_croi){
         if(isset($_SESSION['id']) && $_SESSION['role']==1){
-            
+            echo $id_croi;
+        }
+        else{
+            header('location:../../pages/login');
+            exit();
         }
     }
     
