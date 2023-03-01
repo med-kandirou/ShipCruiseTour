78a4506fe162b6d258d8057f4c3477412aeaf73a
kandirou0 <simo.kandirou15@gmail.com>
2023-03-01 14:05:47 +0100

    update

--- ShipCruiseTour/app/views/reservation.php
+++ ShipCruiseTour/app/views/reservation.php
@@ -44,18 +44,12 @@
  	</div>
  </div>
 
- <div class="container mt-5 mb-5" id="croisieres">
+<div class="container mt-5 mb-5" id="croisieres">
+	
 
 </div>
 
 
-<center>
-	<div id="pagination-numbers">
-
-    </div>
-</center>
-
-    
 
 <?php require_once APPROOT.'/views/inc/footer.php'; ?>
 <script src="<?php echo URLROOT.'/js/user.js' ?>"></script>
